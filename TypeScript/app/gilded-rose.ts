62412cb199c3d3eda6031c87bb8c7ce873a869ec
Nian Teh <Nian.Teh@softwire.com>
2022-07-06 09:09:56 +0100

    Finish refactoring

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -25,10 +25,12 @@ export class GildedRose {
 
       this.items[i].sellIn = this.items[i].sellIn - 1;
 
-      if (this.items[i].name == 'Aged Brie' && this.items[i].quality < 50) {
-        this.items[i].quality = this.items[i].quality + 1;
-      } else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+      switch (this.items[i].name) {
+        case 'Aged Brie':
+          this.items[i].quality = this.items[i].quality + 1;
+          break;
+
+        case 'Backstage passes to a TAFKAL80ETC concert':
           if (this.items[i].sellIn < 0) {
             this.items[i].quality = 0;
           } else if (this.items[i].sellIn < 6) {
@@ -38,35 +40,21 @@ export class GildedRose {
           } else {
             this.items[i].quality = this.items[i].quality + 1;
           }
-        }
+          break;
 
-        if (this.items[i].quality > 50) {
-          this.items[i].quality = 50;
-        }
-      } else {
-        // Do something here to degrade twice as much if after sell in date
-        if (this.items[i].quality > 0) {
-          this.items[i].quality = this.items[i].quality - 1;
-        }
+        default:
+          if (this.items[i].sellIn < 0) {
+            this.items[i].quality = this.items[i].quality - 2;
+          } else {
+            this.items[i].quality = this.items[i].quality - 1;
+          }
       }
 
-
-
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              this.items[i].quality = this.items[i].quality - 1;
-            }
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            // Do we need this line?
-            // this.items[i].quality = this.items[i].quality + 1
-          }
-        }
+      if (this.items[i].quality > 50) {
+        this.items[i].quality = 50;
+      } else if (this.items[i].quality < 0) {
+        this.items[i].quality = 0;
       }
-
     }
 
     return this.items;
